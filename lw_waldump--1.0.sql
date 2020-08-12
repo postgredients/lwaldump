783b998e6fe053b7d8fc35ca09ed202140d99205
Georgy Rylov <g0dj4n@gmail.com>
2020-08-12 11:25:13 +0500

    update control: lwaldump -> lwaldump-10

--- lw_waldump--1.0.sql
+++
@@ -1,7 +0,0 @@
--- complain if script is sourced in psql, rather than via CREATE EXTENSION
-\echo Use "CREATE EXTENSION lwaldump" to load this file. \quit
-
-CREATE FUNCTION lwaldump()
-    RETURNS pg_lsn
-    AS 'MODULE_PATHNAME'
-    LANGUAGE C;
